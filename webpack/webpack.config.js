f66f2b6f2e539df75508d8b59588e467c9ffe3fe
duckit69 <gherab.radouane@gmail.com>
2024-11-10 14:18:06 +0100

    new file:   src/style.css 	modified:   webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,20 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [new HtmlWebPackPlugin()],
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebPackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
-
